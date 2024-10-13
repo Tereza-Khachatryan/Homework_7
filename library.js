7363b2e4b62f3f97b0e2d1831fe7c9360170b05f
Tereza-Khachatryan <saterya97@gmail.com>
2024-10-13 15:31:49 +0400

    Created library file

--- library.js
+++ library.js
@@ -73,3 +73,107 @@ class ReaderBook extends LibraryBookBase {
         return  ` ${this.title}'s expiration Date is ${this.#expirationDate} ,author: ${this.author}.BookID:${this.bookId}, The book is ${returned}`
     }
 }
+
+class Reader {
+    #firstName
+    #lastName
+    #readerId
+    #books
+    constructor(firstName , lastName , readerId , books = []){
+        this.#firstName = firstName
+        this.#lastName = lastName
+        this.#readerId = readerId
+        this.#books = books
+    }
+    get firstName (){
+        return this.#firstName
+    }
+    get lastName (){
+        return this.#lastName
+    }
+    get readerId(){
+        return this.#readerId
+    }
+    get books (){
+        return this.#books
+    }
+    toString(){
+        const bookList = this.#books.length > 0 ? this.#books.map(book => book.toString()).join(', ') :
+        "No books borrowed"
+        return `Name: ${this.#firstName}, lastName: ${this.lastName}, readerId: ${this.#readerId}, books: ${bookList}`
+
+    }
+
+    borrowBook(book, library) {
+        if (book instanceof LibraryBook && library instanceof Library) {
+            const availableBook = library.books.find(b => b.isTheSameBook(book) && b.quantity > 0)
+            if (availableBook) {
+                return library.lendBook(availableBook, this.#readerId)
+            } else {
+                return "Book is not available.";
+            }
+        } else {
+            return "Invalid book or library.";
+        }
+    }
+}
+
+class Library {
+    #books
+    #readers
+    constructor(books = [], readers = []){
+        this.#books = books
+        this.#readers = readers
+    }
+    get books (){
+        return this.#books
+    }
+    get readers (){
+        return this.#readers
+    }
+    doHaveBook (requestedBook){
+        return this.#books.some(
+            (book)=> book.isTheSameBook(requestedBook) && book.quantity > 0)
+    }
+    addBook(newBook){
+        const existingBook = this.#books.find((book) => book.isTheSameBook(newBook))
+        if(existingBook){
+            existingBook.increaseQuantityBy(1)
+        } else {
+            this.#books.push(newBook)
+        }
+        return this.#books
+    }
+    addBooks(newBooks){
+        newBooks.forEach((book) => this.addBook(book))
+        return this.#books
+    }
+    checkReaderId(readerId){
+        return this.#readers.some((reader) => reader.readerId === readerId)
+    }
+
+    lendBook(book, readerId) {
+        const reader = this.#readers.find(reader => reader.readerId === readerId);
+        if (!reader) {
+            console.log("Reader not found.");
+            return null;
+        }
+    
+        if (!this.doHaveBook(book)) {
+            console.log("Book not available.");
+            return null;
+        }
+    
+        const readerBook = new ReaderBook(
+            book.title,
+            book.author,
+            book.bookId,
+            "2024-10-10",
+            false
+        );
+    
+        reader.books.push(readerBook);
+        book.decreaseQuantityBy(1);
+        return readerBook;
+    }
+}
