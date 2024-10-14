1de2feab476111d2e815ea442368f166063ad708
Tereza-Khachatryan <saterya97@gmail.com>
2024-10-14 15:17:37 +0400

    Final version of the game

--- Shiritori.js
+++ Shiritori.js
@@ -1,42 +1,44 @@
 class Shiritory {
+    #words
+    _game_over
     constructor(){
-        this.words = []
-        this.game_over = false
+        this.#words = []
+        this._game_over = false
     }
+
+    get game_over (){
+        return this._game_over
+    }
+
     play (str) {
-        if(this.words.length === 0){
-             this.words.push(str)
-             return this.words
-        }
-        if(this.words.includes(str) || this.words.length === 0){
-            this.game_over = true
-            return "Game is over! This word has already been used"
-        }
-        if(this.words[this.words.length -1].slice(-1) === str[0].toLowerCase()){
-            this.words.push(str)
-            return this.words
-        } else {
-            this.game_over = true
-            return "Game over! The word must start with the last letter of the previous word"
-        }
+        const gameStatus = this._game_over ? "Game over": this.#addWord(str)
+        return gameStatus
     }
+    #addWord(str){
+        this.#words.push(str)
+        return this.#checkGameOver(str)
+    }
+
+    #checkGameOver (str){
+        const lastWord = this.#words[this.#words.length - 2]
+        this._game_over = this.#words.length > 1 && lastWord.slice(-1) !== str[0].toLowerCase()
+        return this._game_over ? "Game over!" : this.#words
+    }
+       
     restart (){
-         this.words.length = 0
-         this.game_over = false
+         this.#words.length = 0
+         this._game_over = false
          return "Game restarted"
     }
 }
 
-let myShiritori = new Shiritory();
-
+let myShiritori = new Shiritory()
 
 console.log(myShiritori.play("apple"))
-console.log(myShiritori.play("Ear"))
-console.log(myShiritori.play("rhino"))
-console.log(myShiritori.play("corn"))
-console.log(myShiritori.restart())
-console.log(myShiritori.words)
-console.log(myShiritori.play("hostess"))
-console.log(myShiritori.play("stash"))
+console.log(myShiritori.play("elephant"))
+console.log(myShiritori.play("tiger"))
+console.log(myShiritori.play("rat"))
+console.log(myShiritori.play("cat"))
+console.log(myShiritori.restart())       
 console.log(myShiritori.play("hostess"))
 
